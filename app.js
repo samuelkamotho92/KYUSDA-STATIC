c1fc33578fa23ee2209773decd546c6b47a9e746
samuel <samuelkamotho92@gmail.com>
2022-03-13 13:03:49 +0300

    did some deletion of files

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    console.log(elems);
-     M.Modal.init(elems);
-  });
-  const sideNav = document.querySelectorAll(".sidenav");
-  M.Sidenav.init(sideNav, {});
-
